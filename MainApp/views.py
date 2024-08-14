ae28249e87308cd7ae8b7f4f61964777f2998b3a
Aleksey Plakhuta <emerax7@gmail.com>
2024-08-14 18:31:26 +0300

    Snippet Forms. Add form...

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,8 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def index_page(request):
@@ -9,7 +11,11 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
+    form = SnippetForm()
+    context = {
+        'pagename': 'Добавление нового сниппета',
+        'form': form,
+    }
     return render(request, 'pages/add_snippet.html', context)
 
 
@@ -20,3 +26,15 @@ def snippets_page(request):
         'snippets': snippets,
     }
     return render(request, 'pages/view_snippets.html', context)
+
+def snippet_detail(request, snippet_id: int):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Snippet with id={snippet_id} not found")
+    else:
+        context = {
+            "pagename": "Просмотр сниппета",
+            "snippet": snippet,
+        }
+        return render(request, "pages/snippet_detail.html", context)
