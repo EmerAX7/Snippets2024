9074669395749b38088086722930c1848c82e3f0
Aleksey Plakhuta <emerax7@gmail.com>
2024-08-16 14:21:35 +0300

    Done. Edit snippet

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ValidationError
+from django.forms import ModelForm, ValidationError, TextInput, Textarea
 from MainApp.models import Snippet
 
 class SnippetForm(ModelForm):
@@ -6,10 +6,15 @@ class SnippetForm(ModelForm):
        model = Snippet
        # Описываем поля, которые будем заполнять в форме
        fields = ['name', 'lang', 'code']
+       labels = {"name": "", "lang": "", "code":""}
+       widgets = {
+           "name": TextInput(attrs={"placeholder": "Название сниппета"}),
+           "code": Textarea(attrs={"placeholder": "Код сниппета"}),
+       }
 
-    # Проверка поля "тфьу"
+    # Проверка поля "name"
     def clean_name(self):
         snippet_name = self.cleaned_data.get("name")
         if len(snippet_name) > 3:
             return snippet_name
-        raise ValidationError("Snippet name too short")
+        raise ValidationError("Snippet name too short") 
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponseNotFound, HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from MainApp.models import Snippet
 from MainApp.forms import SnippetForm
 from django.core.exceptions import ObjectDoesNotExist
@@ -11,12 +11,21 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    form = SnippetForm()
-    context = {
-        'pagename': 'Добавление нового сниппета',
-        'form': form,
-    }
-    return render(request, 'pages/add_snippet.html', context)
+    # Создаём пустую форму при запросе методом GET
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form,
+        }
+        return render(request, 'pages/add_snippet.html', context)
+    # Получаем данные из формы и на их основе создаём новый snippet в БД
+    elif request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request, "pages/add_snippet.html", {'form': form})  # ошибка валидации - отправляем на страницу добавления сниппепа и передаём данные для заполнения формы
 
 
 def snippets_page(request):
@@ -27,6 +36,7 @@ def snippets_page(request):
     }
     return render(request, 'pages/view_snippets.html', context)
 
+
 def snippet_detail(request, snippet_id: int):
     try:
         snippet = Snippet.objects.get(id=snippet_id)
@@ -36,14 +46,47 @@ def snippet_detail(request, snippet_id: int):
         context = {
             "pagename": "Просмотр сниппета",
             "snippet": snippet,
+            "type": 'view',
         }
         return render(request, "pages/snippet_detail.html", context)
 
-def create_snippet(request):
-    from pprint import pprint
-    if request.method == "POST":
-        form = SnippetForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("snippets-list")
-        return render(request, "pages/add_snippet.html", {'form': form})  # ошибка валидации - отправляем на страницу добавления сниппепа и передаём данные для заполнения формы
+
+def snippet_edit(request, snippet_id: int):
+    context = {"pagename": "Редактирование сниппета"}
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        return Http404
+    
+    # Variant 1
+    # Получение данных сниппета с помощью SnippetForm
+    # if request.method == "GET":
+    #     form = SnippetForm(instance=snippet)
+    #     return render(request, "pages/add_snippet.html", {'form': form})
+
+    
+    # Variant 2
+    # Хотим получить страницу с данными сниппета
+    if request.method == "GET":
+        context = {
+            'snippet': snippet,
+            'type': 'edit',
+        }
+        return render(request, 'pages/snippet_detail.html', context)
+    
+    # Получаем данные из формы и на их основе создаём новый snippet в БД
+    elif request.method == "POST":
+        data_form = request.POST
+        snippet.name = data_form["name"]
+        # snippet.lang = data_form["lang"]
+        snippet.code = data_form["code"]
+        # snippet.creation_date = data_form["creation_date"]
+        snippet.save()
+        return redirect("snippets-list")
+
+
+def snippet_delete(request, snippet_id: int):
+    if request.method =="POST":
+        snippet = get_object_or_404(Snippet, id=snippet_id)
+        snippet.delete()
+    return redirect("snippets-list")
